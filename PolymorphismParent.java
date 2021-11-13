c679ac40de2d6c99f71f78cea6919b6f4ffa8644
Akki181999 <93247776+Akki181999@users.noreply.github.com>
2021-11-13 14:45:49 +0530

    Delete PolymorphismParent.java

--- PolymorphismParent.java
+++
@@ -1,7 +0,0 @@
-package com.technoelevate.corejava.assignment;
-
-public class PolymorphismParent {
-	 public void display() {
-		    System.out.println("I am JAVA Programming Language.");
-		  }
-}
