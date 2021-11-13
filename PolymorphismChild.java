87576f7e19b9d39df3001667eac5cb471ec37086
Akki181999 <93247776+Akki181999@users.noreply.github.com>
2021-11-13 14:45:30 +0530

    Delete PolymorphismChild.java

--- PolymorphismChild.java
+++
@@ -1,9 +0,0 @@
-package com.technoelevate.corejava.assignment;
-
-public class PolymorphismChild extends PolymorphismParent {
-	  @Override
-	  public void display() {
-	    System.out.println("I am Object-Oriented Programming Language.");
-	  }
-
-}
